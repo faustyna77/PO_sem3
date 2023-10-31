53a112ecae2cade0e155e3ec55ebbcdb74274680
faustyna77 <faustinmisiura@gmail.com>
2023-10-31 01:33:38 +0100

    rozwiazanie zadań lab1

---
+++ gui_lab1/src/App.java
@@ -0,0 +1,52 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class App extends JFrame {
+    private JPanel JPnel1;
+    private JButton exit;
+    private JEditorPane firstNumber;
+    private JButton addBtn;
+    private JEditorPane secondNumber;
+    private JEditorPane result;
+    private JTextPane textPane1;
+
+    public static void main(String args[])
+      {
+          App App=new App();
+          App.setVisible(true);
+
+
+      }
+
+      public App()
+
+      {
+          super("okno ");
+          this.setContentPane(JPnel1);
+          this.pack();
+          this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+          exit.addActionListener(new ActionListener() {
+              @Override
+              public void actionPerformed(ActionEvent e) {
+                dispose();
+              }
+          });
+          addBtn.addActionListener(new ActionListener() {
+              @Override
+              public void actionPerformed(ActionEvent e) {
+                  String firstNumberText=firstNumber.getText();
+                  String secondNumberText=secondNumber.getText();
+                  int suma=Integer.parseInt(firstNumberText)+Integer.parseInt(secondNumberText);
+                  result.setText(Integer.toString(suma));
+
+
+              }
+          });
+      }
+
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
